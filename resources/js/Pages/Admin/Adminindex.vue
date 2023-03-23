f843775624bc9605f714cdef0cf4bbc31d0a9b7a
wajayanath <wajayanath@gmail.com>
2023-03-23 22:16:05 +0530

    clear layout

--- resources/js/Pages/Admin/Adminindex.vue
+++ resources/js/Pages/Admin/Adminindex.vue
@@ -1,24 +1,20 @@
 <script setup>
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import AdminLayout from "@/Layouts/AdminLayout.vue";
 import { Head } from "@inertiajs/vue3";
 </script>
 
 <template>
     <Head title="Dashboard" />
 
-    <AuthenticatedLayout>
+    <AdminLayout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                 Dashboard
             </h2>
         </template>
 
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                    <div class="p-6 text-gray-900">You're logged in as Administrator !</div>
-                </div>
-            </div>
+        <div class="py-4">
+            <h1>Welcome Admin</h1>
         </div>
-    </AuthenticatedLayout>
+    </AdminLayout>
 </template>
